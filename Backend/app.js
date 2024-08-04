f391b99d28d8a24a331484d6ca51abcfe60cac1e
nishchaljoshi2003 <112483406+nishchaljoshi2003@users.noreply.github.com>
2024-08-04 21:49:44 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
