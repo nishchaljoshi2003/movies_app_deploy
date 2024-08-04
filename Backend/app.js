f3c9e63b915747f73e72b75cd7ccfb32602fc2a3
nishchaljoshi2003 <112483406+nishchaljoshi2003@users.noreply.github.com>
2024-08-04 21:17:49 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://movies-app-api.vercel.app/',
+    origin: 'movies-app-api-git-main-nishchaljoshi2003s-projects.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: '*',
   }));
