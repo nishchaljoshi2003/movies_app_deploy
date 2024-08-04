3b7ee30e0bc7b31a104eebd7f4752b566a0a6777
nishchaljoshi2003 <112483406+nishchaljoshi2003@users.noreply.github.com>
2024-08-04 20:40:40 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: '*',
   }));
